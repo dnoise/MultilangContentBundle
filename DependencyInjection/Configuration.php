4d41db0961ddb8367c7cff06754d77145777c73a
Benedikt Langer <saithis88@gmail.com>
2012-09-03 14:49:20 +0200

    Added validation for the use_sonata_admin configuration value

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -45,7 +45,15 @@ class Configuration implements ConfigurationInterface
                     ))
                     ->prototype('scalar')->end()
                 ->end()
-                ->scalarNode('use_sonata_admin')->defaultValue('auto')->end()
+                ->scalarNode('use_sonata_admin')
+                    ->defaultValue('auto')
+                    ->validate()
+                        ->ifTrue(function($v){
+                            return !is_bool($v) && $v !== 'auto';
+                        })
+                        ->thenInvalid("This configuration allows only the values true, false or 'auto'")
+                    ->end()
+                ->end()
                 ->scalarNode('content_basepath')->defaultNull()->end()
             ->end()
         ;
